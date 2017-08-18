ab927a2925d09721fb741da4e6c72b1aaf0484a5
Jon Skeet <skeet@pobox.com>
2017-08-18 13:27:11 +0100

    wip

--- src/NodaTime.Test/Program.cs
+++ src/NodaTime.Test/Program.cs
@@ -1,6 +1,7 @@
 // Copyright 2017 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
+using NodaTime.Extensions;
 using NodaTime.TimeZones;
 using System;
 using System.Diagnostics;
@@ -12,21 +13,27 @@ namespace NodaTime.Test
     {
         public static void Main(string[] args)
         {
-            var zones = TimeZoneInfo.GetSystemTimeZones().ToList();
-            
-            // Fetch the source outside the measurement time
-            var source = TzdbDateTimeZoneSource.Default;
-            // JIT compile before we start
-            source.GuessZoneIdByTransitionsUncached(TimeZoneInfo.Local);
+            var zones = DateTimeZoneProviders.Tzdb.GetAllZones().Select(Uncache).ToList();
+            var start = Instant.FromUtc(2000, 1, 1, 0, 0);
+            var gap = Duration.FromDays(1);
+            var instants = Enumerable
+                .Range(0, 50000)
+                .Select(index => start + gap * index)
+                .ToList();
             
             var stopwatch = Stopwatch.StartNew();
             foreach (var zone in zones)
             {
-                source.GuessZoneIdByTransitionsUncached(zone);
+                foreach (var instant in instants)
+                {
+                    zone.GetUtcOffset(instant);
+                }
             }
             stopwatch.Stop();
-            Console.WriteLine($"Zones tested: {zones.Count}");
             Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
         }
+        
+        private static DateTimeZone Uncache(DateTimeZone zone) =>
+            zone is CachedDateTimeZone cached ? cached.TimeZone : zone;
     }
 }
