4655276d9e98cdce5b7dabcb72c6af5ba8c9dd3b
Jon Skeet <skeet@pobox.com>
2017-08-18 11:04:24 +0100

    Standalone

--- src/NodaTime.Test/Program.cs
+++ src/NodaTime.Test/Program.cs
@@ -1,16 +1,32 @@
 // Copyright 2017 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
-using NUnitLite;
-using System.Reflection;
+using NodaTime.TimeZones;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace NodaTime.Test
 {
     class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+            var zones = TimeZoneInfo.GetSystemTimeZones().ToList();
+            
+            // Fetch the source outside the measurement time
+            var source = TzdbDateTimeZoneSource.Default;
+            // JIT compile before we start
+            source.GuessZoneIdByTransitionsUncached(TimeZoneInfo.Local);
+            
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var zone in zones)
+            {
+                source.GuessZoneIdByTransitionsUncached(zone);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Zones tested: {zones.Count}");
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
         }
     }
 }
